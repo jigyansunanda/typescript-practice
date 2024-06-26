261e12f330464dc1bfc3e6c0ffb76488e2960a3e
Jigyansu Nanda <nandajigyansu@gmail.com>
2024-06-26 20:17:37 +0530

    protected in typescript

--- day 08/Classes.ts
+++ day 08/Classes.ts
@@ -3,7 +3,7 @@ class User {
     email: string;
     private readonly id: number;
     private _courseCount: number = 1; // private property
-    private isPaidUser: boolean = false; // private property
+    protected isPaidUser: boolean = true; // protected property
 
     constructor(name: string, email: string, id: number) {
         this.name = name;
@@ -58,3 +58,18 @@ user1.logoutUser(); // Outputs: "Token deleted" and "User logged out"
 
 // Trying to access private method directly will cause an error
 // user1.deleteToken(); // This will cause a TypeScript error
+
+class SubUser extends User {
+    isFamily: boolean = true;
+
+    changePaidStatus(): void {
+        this.isPaidUser = false;
+    }
+}
+
+let user2 = new SubUser("Xyz", "xyz@gmail.com", 3251423);
+console.log(user1.getPaidStatus);
+console.log(user2.getPaidStatus);
+user2.changePaidStatus();
+console.log(user1.getPaidStatus);
+console.log(user2.getPaidStatus);
